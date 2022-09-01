766e4016bbfe8fff2bd07df4da427c587d8bbc99
Jim <jimba.ed@outlook.com>
2022-09-01 10:26:49 -0400

    commit tree da7218f0f10219642b7d1abb8e38b1a238257a4f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+    let thankYouMessage = []
+    for (let i=0; i<name.length; i++){
+        thankYouMessage.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouMessage
+}
+writeCards();
+
+function countDown(number){
+   // let integer=number;
+    while (number>=0){
+        console.log(number--)
+    }
+}
